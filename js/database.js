af7c2500ad53689b6610a21e5e926299cded50b3
Wendell Gutierrez <wengut.alm@gmail.com>
2022-09-13 23:11:36 -0300

    Atualizando database JS e arquivos de imagens para os produtos

--- js/database.js
+++ js/database.js
@@ -3,62 +3,113 @@
 const data = [
   {
     id: 1,
-    img: "../img/jaqueta.svg",
-    nameItem: "Lightweight Jacket",
+    img: '../img/fob-cd-cover.jpg',
+    nameItem: 'Fall Out Boy - From Under the Cork Tree',
+    artist: 'Fall Out Boy',
     description:
-      "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+      "From Under the Cork Tree is the second studio album by the American rock band Fall Out Boy, released on May 3, 2005, through Island Records as the band's major label debut.",
+    year: 2005,
+    value: 50,
+    addCart: 'Add to Cart',
+    tag: ['CD']
   },
   {
     id: 2,
-    img: "../img/gorro.svg",
-    nameItem: "Black Hat",
+    img: '../img/fob-lp-cover.jpeg',
+    nameItem:
+      'Fall Out Boy - Take This To Your Grave [Fueled by Ramen 25th Anniversary Edition Silver Vinyl]',
+    artist: 'Fall Out Boy',
     description:
-      "O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Acessórios"],
+      'Limited silver colored vinyl LP pressing. Take This to Your Grave is the debut studio album by rock band Fall Out Boy, originally released on May 6, 2003.',
+    year: 2021,
+    value: 250,
+    addCart: 'Add to Cart',
+    tag: ['LP']
   },
   {
     id: 3,
-    img: "../img/mascara.svg",
-    nameItem: "Mask",
+    img: '../img/fob-dvd-cover.jpg',
+    nameItem: 'Fall Out Boy - The Boys Of Zummer Tour',
+    artist: 'Fall Out Boy',
     description:
-      "Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...",
-    value: 40,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Acessórios"],
+      'Join multi-platinum band Fall Out Boy as they travel back to their hometown of Chicago for a mind-blowing audio-visual extravaganza.',
+    year: 2016,
+    value: 89,
+    addCart: 'Add to Cart',
+    tag: ['DVD']
   },
   {
     id: 4,
-    img: "../img/camiseta_preta.svg",
-    nameItem: "T-Shirt",
+    img: '../img/mcr-cd-cover.jpg',
+    nameItem: 'My Chemical Romance - Three Cheers for Sweet Revenge',
+    artist: 'My Chemical Romance',
     description:
-      "Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+      'Three Cheers for Sweet Revenge is the second studio album by American rock band My Chemical Romance, released on June 8, 2004, by Reprise Records.',
+    year: 2004,
+    value: 50,
+    addCart: 'Add to Cart',
+    tag: ['CD']
   },
   {
     id: 5,
-    img: "../img/camiseta_branca.svg",
-    nameItem: "Short-Sleeve T-Shirt",
+    img: '../img/mcr-lp-cover.jpg',
+    nameItem:
+      'My Chemical Romance - Black Parade / Living With Ghosts (10th Anniversary Edition)',
+    artist: 'My Chemical Romance',
     description:
-      "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+      "2016 triple vinyl LP pressing. LP One and LP Two of this release contain a reissue of My Chemical Romance's double-platinum, genre-defining album The Black Parade, which was originally released by Reprise in October 2006.",
+    year: 2016,
+    value: 299,
+    addCart: 'Add to Cart',
+    tag: ['LP']
   },
   {
     id: 6,
-    img: "../img/moletom.svg",
-    nameItem: "Champion Packable Jacket",
+    img: '../img/mcr-dvd-cover.jpg',
+    nameItem: 'My Chemical Romance - The Black Parade is Dead',
+    artist: 'My Chemical Romance',
     description:
-      "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+      'After years of touring, the band offers two incredible concerts on DVD as they move forward musically to ceremoniously end the era of The Black Parade.',
+    year: 2008,
+    value: 99,
+    addCart: 'Add to Cart',
+    tag: ['DVD']
   },
-];
+  {
+    id: 7,
+    img: '../img/trja-cd-cover.jpg',
+    nameItem: "The Red Jumpsuit Apparatus - Don't You Fake It",
+    artist: 'The Red Jumpsuit Apparatus',
+    description:
+      'Don\'t You Fake It is the debut studio album by American rock band The Red Jumpsuit Apparatus. The title is taken from a line in the opening track, "In Fate\'s Hands".',
+    year: 2006,
+    value: 65,
+    addCart: 'Add to Cart',
+    tag: ['CD']
+  },
+  {
+    id: 8,
+    img: '../img/paramore-lp-cover.jpeg',
+    nameItem:
+      'Paramore - All We Know Is Falling [Fueled by Ramen 25th Anniversary Edition Silver Vinyl]',
+    artist: 'Paramore',
+    description:
+      'Limited silver colored vinyl LP pressing. All We Know Is Falling is the debut studio album by Paramore, originally released in 2005.',
+    year: 2021,
+    value: 250,
+    addCart: 'Add to Cart',
+    tag: ['LP']
+  },
+  {
+    id: 9,
+    img: '../img/fresno-dvd-cover.jpg',
+    nameItem: 'Fresno - 15 Anos Ao Vivo',
+    artist: 'Fresno',
+    description:
+      "October 16, 2014, Audio Club: an unforgettable day for Lucas Silveira, Gustavo Mantovani, Mario Camelo, Thiago Pessoa and all Fresno fans. The group played hits like 'Alguém Que Te Faz Te Sorrir', 'Desde Quando Você Se Foi' and 'Quebre as Correntes', the song that consecrated the group in 2006.",
+    year: 2014,
+    value: 70.9,
+    addCart: 'Add to Cart',
+    tag: ['DVD']
+  }
+]
