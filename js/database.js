98b1b98485a6fd6ac68dc26a25ca3f5cd703fa23
Wendell Gutierrez <wengut.alm@gmail.com>
2022-09-14 20:42:03 -0300

    Finalizado, funcional, porém incompleto. Preciso melhorar algumas funções e realizar o desafio de filtros também

--- js/database.js
+++ js/database.js
@@ -4,10 +4,10 @@ const data = [
   {
     id: 1,
     img: '../img/fob-cd-cover.jpg',
-    nameItem: 'Fall Out Boy - From Under the Cork Tree',
+    nameItem: 'From Under the Cork Tree',
     artist: 'Fall Out Boy',
     description:
-      "From Under the Cork Tree is the second studio album by the American rock band Fall Out Boy, released on May 3, 2005, through Island Records as the band's major label debut.",
+      'From Under the Cork Tree is the second studio album by the American rock band Fall Out Boy, released on May 3, 2005...',
     year: 2005,
     value: 50,
     addCart: 'Add to Cart',
@@ -16,8 +16,7 @@ const data = [
   {
     id: 2,
     img: '../img/fob-lp-cover.jpeg',
-    nameItem:
-      'Fall Out Boy - Take This To Your Grave [Fueled by Ramen 25th Anniversary Edition Silver Vinyl]',
+    nameItem: 'Take This To Your Grave',
     artist: 'Fall Out Boy',
     description:
       'Limited silver colored vinyl LP pressing. Take This to Your Grave is the debut studio album by rock band Fall Out Boy, originally released on May 6, 2003.',
@@ -29,7 +28,7 @@ const data = [
   {
     id: 3,
     img: '../img/fob-dvd-cover.jpg',
-    nameItem: 'Fall Out Boy - The Boys Of Zummer Tour',
+    nameItem: 'The Boys Of Zummer Tour',
     artist: 'Fall Out Boy',
     description:
       'Join multi-platinum band Fall Out Boy as they travel back to their hometown of Chicago for a mind-blowing audio-visual extravaganza.',
@@ -41,8 +40,8 @@ const data = [
   {
     id: 4,
     img: '../img/mcr-cd-cover.jpg',
-    nameItem: 'My Chemical Romance - Three Cheers for Sweet Revenge',
-    artist: 'My Chemical Romance',
+    nameItem: 'Three Cheers for Sweet Revenge',
+    artist: 'MCR',
     description:
       'Three Cheers for Sweet Revenge is the second studio album by American rock band My Chemical Romance, released on June 8, 2004, by Reprise Records.',
     year: 2004,
@@ -53,11 +52,10 @@ const data = [
   {
     id: 5,
     img: '../img/mcr-lp-cover.jpg',
-    nameItem:
-      'My Chemical Romance - Black Parade / Living With Ghosts (10th Anniversary Edition)',
-    artist: 'My Chemical Romance',
+    nameItem: 'Black Parade / Living With Ghosts',
+    artist: 'MCR',
     description:
-      "2016 triple vinyl LP pressing. LP One and LP Two of this release contain a reissue of My Chemical Romance's double-platinum, genre-defining album The Black Parade, which was originally released by Reprise in October 2006.",
+      "2016 triple vinyl LP pressing. LP One and LP Two of this release contain a reissue of My Chemical Romance's double-platinum,...",
     year: 2016,
     value: 299,
     addCart: 'Add to Cart',
@@ -65,9 +63,9 @@ const data = [
   },
   {
     id: 6,
-    img: '../img/mcr-dvd-cover.jpg',
-    nameItem: 'My Chemical Romance - The Black Parade is Dead',
-    artist: 'My Chemical Romance',
+    img: '../img/mcr-dvd-cover.png',
+    nameItem: 'The Black Parade is Dead',
+    artist: 'MCR',
     description:
       'After years of touring, the band offers two incredible concerts on DVD as they move forward musically to ceremoniously end the era of The Black Parade.',
     year: 2008,
@@ -78,10 +76,10 @@ const data = [
   {
     id: 7,
     img: '../img/trja-cd-cover.jpg',
-    nameItem: "The Red Jumpsuit Apparatus - Don't You Fake It",
-    artist: 'The Red Jumpsuit Apparatus',
+    nameItem: "Don't You Fake It",
+    artist: 'TRJA',
     description:
-      'Don\'t You Fake It is the debut studio album by American rock band The Red Jumpsuit Apparatus. The title is taken from a line in the opening track, "In Fate\'s Hands".',
+      "Don't You Fake It is the debut studio album by American rock band The Red Jumpsuit Apparatus. The title is taken from a...",
     year: 2006,
     value: 65,
     addCart: 'Add to Cart',
@@ -90,8 +88,7 @@ const data = [
   {
     id: 8,
     img: '../img/paramore-lp-cover.jpeg',
-    nameItem:
-      'Paramore - All We Know Is Falling [Fueled by Ramen 25th Anniversary Edition Silver Vinyl]',
+    nameItem: 'All We Know Is Falling',
     artist: 'Paramore',
     description:
       'Limited silver colored vinyl LP pressing. All We Know Is Falling is the debut studio album by Paramore, originally released in 2005.',
@@ -103,10 +100,10 @@ const data = [
   {
     id: 9,
     img: '../img/fresno-dvd-cover.jpg',
-    nameItem: 'Fresno - 15 Anos Ao Vivo',
+    nameItem: 'Tour 15 Anos Ao Vivo',
     artist: 'Fresno',
     description:
-      "October 16, 2014, Audio Club: an unforgettable day for Lucas Silveira, Gustavo Mantovani, Mario Camelo, Thiago Pessoa and all Fresno fans. The group played hits like 'Alguém Que Te Faz Te Sorrir', 'Desde Quando Você Se Foi' and 'Quebre as Correntes', the song that consecrated the group in 2006.",
+      'October 16, 2014, Audio Club: an unforgettable day for Lucas Silveira, Gustavo Mantovani, Mario Camelo, Thiago Pessoa and all Fresno...',
     year: 2014,
     value: 70.9,
     addCart: 'Add to Cart',
--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 let showcase = document.getElementsByClassName('showcase')[0]
+let cardButton = document.getElementsByClassName('product-button')
+let removeButton = document.getElementsByClassName('remove-button')
+let emptyCart = document.getElementsByClassName('empty-cart')[0]
+let cartQuantity = 0
+let cartTotal = 0
 
 createCardFromList(data, showcase)
+addToCart()
 
 function createCard(product) {
+  let id = product.id
   let img = product.img
   let category = product.tag[0]
   let name = product.nameItem
@@ -54,6 +61,7 @@ function createCard(product) {
   let productButton = document.createElement('button')
   productButton.classList.add('product-button')
   productButton.innerText = button
+  productButton.id = id
 
   productInfo.append(productArtist, productYear, productCategory)
   productCard.append(
@@ -68,6 +76,49 @@ function createCard(product) {
   return productCard
 }
 
+function createCartItem(product) {
+  emptyCart.innerHTML = ''
+
+  let img = product.img
+  let title = product.nameItem
+  let price = product.value
+
+  let cartItem = document.createElement('li')
+  cartItem.classList.add('cart-item')
+
+  let itemCartImg = document.createElement('img')
+  itemCartImg.classList.add('cart-img')
+  itemCartImg.src = img
+  itemCartImg.alt = `Cover of ${title}`
+  itemCartImg.title = title
+
+  let cartInfo = document.createElement('div')
+  cartInfo.classList.add('cart-info')
+  let cartAlign = document.createElement('div')
+  cartAlign.classList.add('cart-align')
+  let itemCartTitle = document.createElement('h1')
+  itemCartTitle.classList.add('cart-item-title')
+  itemCartTitle.innerText = title
+
+  let productPrice = document.createElement('strong')
+  productPrice.classList.add('product-price')
+  productPrice.innerText = `$ ${price}`
+
+  let remove = document.createElement('button')
+  remove.classList.add('remove-button')
+  remove.innerText = 'Remove from cart'
+  remove.addEventListener('click', function (event) {
+    let item = event.path[2]
+    item.remove()
+    cartQuantity = 0
+    cartTotal = 0
+  })
+  cartInfo.append(itemCartTitle, productPrice)
+  cartAlign.append(cartInfo, remove)
+  cartItem.append(itemCartImg, cartAlign)
+  return cartItem
+}
+
 function createCardFromList(list, destinationElement) {
   destinationElement.innerHTML = ''
 
@@ -80,10 +131,48 @@ function createCardFromList(list, destinationElement) {
   }
 }
 
-function addToCart() {}
+function addToCart() {
+  for (let i = 0; i < data.length; i++) {
+    let button = cardButton[i]
 
-function removeFromCart() {}
+    button.addEventListener('click', function (event) {
+      let productId = parseInt(event.currentTarget.id)
+      let cartItem = findItem(productId)
+      let newItem = createCartItem(cartItem)
+      let cart = document.createElement('ul')
+      cart.classList.add('cart-loaded')
+      cart.append(newItem)
 
-function filterCards() {}
+      cartQuantity++
+      let quantity = document.createElement('div')
+      let textQuantity = document.createElement('h3')
+      textQuantity.innerText = 'Quantity:'
 
-function findCard() {}
+      let itemQuantity = document.createElement('p')
+      itemQuantity.innerText = `${cartQuantity}`
+      quantity.append(textQuantity, itemQuantity)
+      quantity.classList.add('cart-quantity')
+
+      cartTotal += cartItem.value
+      let total = document.createElement('div')
+      let textTotal = document.createElement('h3')
+      textTotal.innerText = 'Total:'
+      let itemsTotal = document.createElement('p')
+      itemsTotal.innerText = `$ ${cartTotal}`
+      total.append(textTotal, itemsTotal)
+      total.classList.add('cart-total')
+
+      emptyCart.append(cart, quantity, total)
+    })
+  }
+}
+
+function findItem(id) {
+  for (let i = 0; i < data.length; i++) {
+    let product = data[i]
+    if (product.id == id) {
+      return product
+    }
+  }
+  return false
+}
